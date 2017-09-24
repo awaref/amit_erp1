8fb50da2bb5e849b8efe5c80f9b9fe6c6613d9f8
Mohab Hamdy <mohab.hamdy123@gmail.com>
2017-09-24 21:45:46 +0200

    Add: add_products, some changes

---
+++ add_products.php
@@ -0,0 +1,46 @@
+<?php
+include 'includes/header.php';
+include 'includes/classes/config/Crud.php';
+include 'includes/classes/config/Validation.php';
+?>
+<?php include 'includes/main_header.php'; ?>
+<?php include 'includes/left_sidebar.php'; ?>
+
+	<!-- Content Wrapper. Contains page content -->
+	<div class="content-wrapper">
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  	<h1>
+	  		ERP
+	    	<small>Add User</small>
+	  	</h1>
+	</section>
+<!-- Main row -->
+		<div class="row">
+			<div class="add-user-form">
+				<div class="col-md-6 col-md-offset-3">
+					<form action="add_user.php" method="POST">
+					    
+					    
+					</form>
+				</div>
+			</div>
+		</div>
+		<!-- /.row (main row) -->
+	</section>
+	<!-- /.content -->
+
+	<?php //include 'includes/main_content.php'; ?>
+	</div>
+	<!-- /.content-wrapper -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'includes/main_footer.php'; ?>
--- includes/left_sidebar.php
+++ includes/left_sidebar.php
@@ -45,8 +45,8 @@
 			    	</span>
 		  		</a>
 		  		<ul class="treeview-menu">
+		  			<li><a href="add_products.php"><i class="fa fa-circle-o"></i> Add Products</a></li>
 		    		<li><a href="index.html"><i class="fa fa-circle-o"></i> List Products</a></li>
-		    	<li><a href="index2.html"><i class="fa fa-circle-o"></i> Add Products</a></li>
 		  		</ul>
 			</li>
 		</ul>
