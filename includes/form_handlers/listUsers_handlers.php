3f7e6b0783ee4b5b6f262f89af8eb043437c52e2
awaref <32125024+awaref@users.noreply.github.com>
2017-09-24 14:19:01 +0200

    small edit
    -session remove from handeler

--- includes/form_handlers/listUsers_handlers.php
+++ includes/form_handlers/listUsers_handlers.php
@@ -1,10 +1,10 @@
 <?php 
-  session_start();
+  
   $crud = new Crud();
 
   $query = "SELECT * FROM `users`";
   $result = $crud->getData($query); 
- 
+
   if ($result === false) 
   {
     echo "Query cannot be excuted";
